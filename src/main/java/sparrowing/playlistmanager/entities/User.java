8219f4f98c158d06cb78560570266a9fd79cd5da
Sparrow <mel.e.grigg@gmail.com>
2017-12-13 18:24:21 -0600

    Deleted entity boilerplates, began User entity construction, fixed security dependencies not to block the application loading

--- src/main/java/sparrowing/playlistmanager/entities/User.java
+++ src/main/java/sparrowing/playlistmanager/entities/User.java
@@ -1,81 +1,64 @@
 package sparrowing.playlistmanager.entities;
 
 import java.util.Date;
-import java.util.List;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-import javax.validation.constraints.NotNull;
-
-@Entity
-@Table(name = "users_test")
 public class User {
 	
 	/*
-	  int             id         Unique User id
-	  String          username   Username of user
-	  String          pwHash     The user's hashed password
-	  Date            joined     Date and time that user joined
-	  List<Playlist>  playlists  Playlists owned by user
-	  List<Song>      songs      User's song entries
+	 * int userId - Unique user ID number
+	 * String username - User's username
+	 * Date dateTimeJoined - Time object of when the user first was created
 	 */
 	
-	// PROPERTIES --------------------------------------------------------
+	private int userId;
 	
-	@Id @GeneratedValue @NotNull
-	@Column(name = "user_id", unique = true)
-	private int id;
-	
-	@NotNull
-	@Column(name = "username")
 	private String username;
 	
-	@NotNull
-	@Column(name = "pw_hash")
-	private String pwHash;
+	private Date dateTimeJoined;
 	
-	@Temporal(TemporalType.TIMESTAMP) @NotNull
-	@Column(name = "joined")
-	private Date joined;
+	// CONSTRUCTORS ==============================================
 	
-	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "owner")
-	private List<Playlist> playlists;
+	@SuppressWarnings("unused")
+	private User() {
+		// Empty constructor for Hibernate
+	}
 	
-	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "creator")
-	private List<Song> songs;
+	public User(String username) {
+		this.username = username;
+	}
 	
-	// CONSTRUCTORS ------------------------------------------------------
+	// GETTERS ===================================================
 	
-	public User() { }
+	public int getUserId() {
+		return this.userId;
+	}
 	
-	// PRIVATE METHODS ---------------------------------------------------
+	public String getUsername() {
+		return this.username;
+	}
 	
-	// PUBLIC STATIC METHODS ---------------------------------------------
+	public Date getDateTimeJoined() {
+		return this.dateTimeJoined;
+	}
 	
-	// PUBLIC INSTANCE METHODS -------------------------------------------
+	// SETTERS ===================================================
 	
-	// GETTERS AND SETTERS -----------------------------------------------
+	// Setters are required for Hibernate - these set to private because
+	//    outside use is not wanted
 	
-	public int getId()                   { return this.id; }
-	public String getUsername()          { return this.username; }
-	public String getPwHash()            { return this.pwHash; }
-	public Date getJoined()              { return this.joined; }
-	public List<Playlist> getPlaylists() { return this.playlists; }
-	public List<Song> getSongs()         { return this.songs; }
+	@SuppressWarnings("unused")
+	private void setUserId(int userId) {
+		this.userId = userId;
+	}
 	
-	@SuppressWarnings("unused") private void setId(int id)                          { this.id = id; }
-	@SuppressWarnings("unused") private void setUsername(String username)           { this.username = username; }
-	@SuppressWarnings("unused") private void setPwHash(String pwHash)               { this.pwHash = pwHash; }
-	@SuppressWarnings("unused") private void setJoined(Date joined)                 { this.joined = joined; }
-	@SuppressWarnings("unused") private void setPlaylists(List<Playlist> playlists) { this.playlists = playlists; }
-	@SuppressWarnings("unused") private void setSongs(List<Song> songs)             { this.songs = songs; }
+	@SuppressWarnings("unused")
+	private void setUsername(String username) {
+		this.username = username;
+	}
+	
+	@SuppressWarnings("unused")
+	private void setDateTimeJoined(Date dateTimeJoined) {
+		this.dateTimeJoined = dateTimeJoined;
+	}
 	
 }
