7017111ff07b4acf6ecb248926ac2c8e72f2e095
Sparrow <mel.e.grigg@gmail.com>
2017-12-16 23:47:34 -0600

    Added some util functions needed for User and Form objects

--- src/main/java/sparrowing/playlistmanager/util/Functions.java
+++ src/main/java/sparrowing/playlistmanager/util/Functions.java
@@ -5,13 +5,27 @@ import java.util.regex.Pattern;
 
 public class Functions {
 	
-	/*
-	 * Returns whether or not String match matches the supplied regex pattern
+	/**
+	 * Returns whether or not a String matches a supplied regular expression pattern
+	 * 
+	 * @param regex Regex string to check against
+	 * @param match String to check against the regular expression
+	 * @return True if String matches the pattern, else false
 	 */
 	public static boolean isRegexMatch(String regex, String match) {
 		Pattern pattern = Pattern.compile(regex);
 		Matcher matcher = pattern.matcher(match);
 		return matcher.matches();
+	}
+	
+	/**
+	 * Small utility function to determine if a String is completely empty (or null)
+	 * 
+	 * @param s String to be checked
+	 * @return True if String is empty, else false
+	 */
+	public static boolean isStringEmpty(String s) {
+		return s == null || s == "";
 	}
 
 }
